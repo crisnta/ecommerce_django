9c316115cd4f557125eb00b15b6393a521a7eaad
crisnta <torresalvarezcristobal@gmail.com>
2022-10-28 19:08:01 -0300

    rx Product

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,12 +1,44 @@
-import Layout from '../hocs/Layout'
+import Layout from "../hocs/Layout"
+import { connect } from 'react-redux';
+import { 
+    get_products_by_arrival, 
+    get_products_by_sold 
+} from '../redux/actions/product';
+import { useEffect } from "react";
+import Banner from "../components/home/Banner"
+import ProductsSold from "../components/home/ProductsSold"
+import ProductsArrival from "../components/home/ProductsArrival"
 
-const Home = () =>{
-    return ( 
+
+const Home = ({
+    get_products_by_arrival, 
+    get_products_by_sold,
+    products_arrival,
+    products_sold
+}) => {
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+
+        get_products_by_arrival();
+        get_products_by_sold();
+    }, []);
+
+    return(
         <Layout>
-            <div className='text-blue-500'>
-                Home
-            </div>
+            <Banner/>
+            <ProductsArrival data={products_arrival}/> 
+            <ProductsSold data={products_sold}/>
         </Layout>
     )
 }
-export default Home
+
+const mapStateToProps = state => ({
+    products_arrival: state.Products.products_arrival,
+    products_sold: state.Products.products_sold,
+})
+
+export default connect(mapStateToProps,{
+    get_products_by_arrival, 
+    get_products_by_sold,
+}) (Home)
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,9 +2,10 @@ import { combineReducers } from "redux";
 import Auth from "./auth";
 import Alert from "./alert"
 import Categories from "./categories";
-
+import Products from "./product";
 export default combineReducers({
     Auth, 
     Alert,
-    Categories
+    Categories,
+    Products,
 })
