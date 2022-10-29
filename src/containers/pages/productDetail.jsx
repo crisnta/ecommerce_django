25b021c6e7f7b8547c8959f9f3c59ca9725ecb07
crisnta <torresalvarezcristobal@gmail.com>
2022-10-29 18:01:27 -0300

    fixed scroll

--- src/containers/pages/productDetail.jsx
+++ src/containers/pages/productDetail.jsx
@@ -61,6 +61,7 @@ const ProductDetail = ({
     const productId = params.productId
 
     useEffect(() => {
+      window.scrollTo(0,0)
       get_product(productId)
       get_related_products(productId)
       
